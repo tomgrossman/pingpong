001451d4d832b18f1936355fef1524ca58c7fed4
tomgrossman <tomg1988@gmail.com>
2017-08-06 10:07:59 +0300

    added routes

---
+++ frontend/app.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const express       = require('express');
+const bodyParser    = require('body-parser');
+
+const LoginRoute    = require('./routes/login');
+const RegisterRoute = require('./routes/register');
+const TableRoute    = require('./routes/table');
+
+const appServer = express();
+
+const LIMIT = '6mb';
+
+//	For Parsing application/json
+appServer.use(
+    bodyParser.json(
+        {
+            limit: LIMIT
+        }
+    )
+);
+
+//	For Parsing application/x-www-form-urlencoded
+appServer.use(
+    bodyParser.urlencoded(
+        {
+            limit: LIMIT,
+            extended: true
+        }
+    )
+);
+
+appServer.use(
+    '/login',
+    LoginRoute
+);
+
+appServer.use(
+    '/register',
+    RegisterRoute
+);
+
+appServer.use(
+    '/table',
+    TableRoute
+)
+
+appServer.listen(
+    3333,
+    '0.0.0.0',
+    function () {
+        console.log(new Date() + ' Listening on port 3333');
+    }
+);
