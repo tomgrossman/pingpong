4c1627078d2d2603cb7a925238c42aec334ece31
Adam Delman <flyn@flyn.cc>
2017-08-06 12:23:56 +0300

    wip

--- backend/db.py
+++ backend/db.py
@@ -1,5 +1,6 @@
 import pymongo
 import pprint
+import bson.objectid
 
 class PlayersDB:
     def __init__(self):
@@ -13,7 +14,6 @@ class PlayersDB:
         except Exception as e:
             print(str(e))
 
-
     def update_player_info(
         self,
         _id,
@@ -23,10 +23,10 @@ class PlayersDB:
         points,
     ):
         try:
-            self.mongo_session.Players.update_one(
-                {"_id": _id},
+            self.mongo_session.users.update_one(
+                {'_id': _id},
                 {
-                    "$set": {
+                    '$set': {
                             'email': email,
                             'full_name': full_name,
                             'team': team,
@@ -42,7 +42,7 @@ class PlayersDB:
         _id,
         ):
             try:
-                self.mongo_session.Players.delete_many({"_id":_id})
+                self.mongo_session.users.delete_many({'_id':_id})
             except Exception as e:
                 print(str(e))
 
@@ -60,13 +60,69 @@ class PlayersDB:
         self,
         user_id,
     ):
-        return self.mongo_session.users.find_one({"_id": user_id})
+        return self.mongo_session.users.find_one(
+            filter={
+                '_id': user_id,
+            },
+        )
 
-def main():
-    db = PlayersDB()
-    # pprint.pprint(db.get_all_players())
-    pprint.pprint(db.get_all_played_matches())
+    def update_points_for_match(
+        self,
+        users_id_to_points,
+    ):
+
+        for user_id,points in users_id_to_points.items():
+            user = self.get_user_by_id(
+                user_id=user_id,
+            )
+            user['points'] += points
+            self.mongo_session.users.update(
+                {
+                    '_id': bson.objectid.ObjectId(user_id),
+                },
+                user,
+            )
+
+    def get_match_by_id(
+        self,
+        match_id,
+    ):
+        match = self.mongo_session.matches.find_one(
+            filter={
+                '_id': match_id,
+            },
+        )
+        return match
+
+    def get_user_by_id(
+        self,
+        user_id,
+    ):
+        user = self.mongo_session.users.find_one(
+            filter={
+                '_id': user_id,
+            },
+        )
+        return user
+
+    def finalize_match(
+        self,
+        match_id,
+    ):
+        match = self.get_match_by_id(
+            match_id=match_id,
+        )
+        match['status'] = 'points_assigned'
+        self.mongo_session.matches.update(
+            {
+                '_id': bson.objectid.ObjectId(match_id),
+            },
+            match,
+        )
 
 
 if __name__ == '__main__':
-    main()
+    db = PlayersDB()
+    # pprint.pprint(db.get_all_players())
+    match = db.get_all_played_matches()[0]
+    # db.finalize_match(match['_id'])
