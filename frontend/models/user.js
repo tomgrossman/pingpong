37672ebeeca0599996012a0a6087c1c5ff347512
liba <liba@intsights.com>
2017-08-10 17:14:06 +0300

    change new users points to new rankings system

--- frontend/models/user.js
+++ frontend/models/user.js
@@ -35,7 +35,7 @@ const UserShchema = new Schema(
         points: {
             type: Number,
             required: true,
-            default: 0
+            default: 1000
         }
     },
     {
